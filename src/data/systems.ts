621ec4fe3cab1148f53e79afc3cbbcb1f1ee8c1b
SuperMustard <mr.jiahanxin@gmail.com>
2026-02-09 20:56:42 -0500

    rearrange assets folder

--- src/data/systems.ts
+++ src/data/systems.ts
@@ -1,10 +1,1 @@
-import skeletons from "./skeleton.json";
 
-export const SYSTEMS = {
-  skeleton: {
-    name: "Skeletons",
-    data: skeletons,
-  },
-};
-
-export type SystemKey = keyof typeof SYSTEMS;
